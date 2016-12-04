bb536561aa6b83ccac7c6daff5b26efea30b5a20
YangXiaochun-admin <13098858665@qq.com>
2016-12-05 02:50:37 +0800

    newsApp

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(function(){
+    function size(originSize,type){
+        var type=type || "x";
+        if(type=="x"){
+            var widths=document.documentElement.clientWidth;
+            var scale=widths/originSize*100;
+        }else if(type=="y"){
+            var heights=document.documentElement.clientHeight;
+            var scale=heights/originSize*100;
+        }
+
+        document.getElementsByTagName("html")[0].style.fontSize=scale+"px";
+    }
+    size(750);
+//			若是按y轴 siz(1334,y)
+    var mySwiper = new Swiper('.swiper-container',{
+        pagination : '.swiper-pagination',
+        paginationHide :true,
+        onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+            swiperAnimateCache(swiper); //隐藏动画元素
+            swiperAnimate(swiper); //初始化完成开始动画
+        },
+        onSlideChangeEnd: function(swiper){
+            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+        }
+    })
+
+})
